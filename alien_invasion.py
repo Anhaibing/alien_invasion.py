11dc526541a84c717a46fd6cc5beb239cc57cad8
Anhaibing <haibing.an1991@gmail.com>
2017-11-20 14:19:50 +0800

    2017-11-20 alien_invasion
    1.add bullet to game
    2.space-key got fire
    
    Signed-off-by: Anhaibing <haibing.an1991@gmail.com>

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_function as gf
+from pygame.sprite import Group
 
 def run_game():
 	#pygame init
@@ -17,10 +18,14 @@ def run_game():
 	#make a ship
 	ship = Ship(ai_settings, screen)
 
+	#创建一个用于存储子弹的编组
+	bullets = Group()
+
 	#cycle
 	while True:
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		bullets.update()
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- game_function.py
+++ game_function.py
@@ -1,12 +1,17 @@
 import sys
 import pygame
-
-def check_keydown_events(event, ship):
+from bullet import Bullet
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
 	if event.key == pygame.K_RIGHT:
 	#检测右键按下，并向右移动
 		ship.moving_right = True
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = True
+
+	elif event.key == pygame.K_SPACE:
+		#创建一颗子弹，将其加入大编组bullets中
+		new_bullet = Bullet(ai_settings, screen, ship)
+		bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
 	if event.key == pygame.K_RIGHT:
@@ -14,7 +19,7 @@ def check_keyup_events(event, ship):
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = False
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
 	#key & mouse event
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -22,16 +27,20 @@ def check_events(ship):
 
 			#按下按键
 			elif event.type == pygame.KEYDOWN:
-				check_keydown_events(event, ship)
+				check_keydown_events(event, ai_settings, screen, ship, bullets)
 
 			#松开按键
 			elif event.type == pygame.KEYUP:
 				check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
 	#redraw screen
-		screen.fill(ai_settings.bg_color)
-		#draw ship
-		ship.blitme()
-		#flash screen
-		pygame.display.flip()
+	screen.fill(ai_settings.bg_color)
+
+	#在飞船和外星人后面重回所有子弹
+	for bullet in bullets.sprites():
+		bullet.draw_bullet()
+	#draw ship
+	ship.blitme()
+	#flash screen
+	pygame.display.flip()
